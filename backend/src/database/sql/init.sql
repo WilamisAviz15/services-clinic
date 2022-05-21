5a83f182639d26a54ca222d231751e8e5e37589a
wilamisaviz15 <wmaa@ic.ufal.br>
2022-05-20 23:44:06 -0300

    Refactor medical care files: db and route to SQL standard

--- backend/src/database/sql/init.sql
+++ backend/src/database/sql/init.sql
@@ -30,15 +30,15 @@ CREATE TABLE IF NOT EXISTS application_medical_services(
 
 CREATE TABLE IF NOT EXISTS application_medical_appointment(
     uuid uuid DEFAULT uuid_generate_v4(),
-    userId uuid,
-    medicalServiceId uuid,
+    user_id uuid,
+    medical_service_id uuid,
     PRIMARY KEY (uuid),
-    FOREIGN KEY (userId) REFERENCES "application_user" (uuid),
-    FOREIGN KEY (medicalServiceId) REFERENCES "application_medical_services" (uuid)
+    FOREIGN KEY (user_id) REFERENCES "application_user" (uuid),
+    FOREIGN KEY (medical_service_id) REFERENCES "application_medical_services" (uuid)
 )
 
 INSERT INTO application_user(username, password, userType) VALUES ('admin', crypt('admin', 'secret'), 'administrador')
 INSERT INTO application_doctor(name, scholarity, balance_to_receive) VALUES ('Dr. Ave Maria', 'Doutor em Medicina', '5000.00')
 INSERT INTO application_medical_services(name, value, duration, doctor_id) VALUES ('Clinico Geral', '60.00', 10, 'ab27a220-8f8a-4032-99ff-f1b5a4b84425')
-INSERT INTO application_medical_appointment(userId, medicalServiceId) 
+INSERT INTO application_medical_appointment(user_id, medical_service_id) 
 VALUES ('f90bf00e-c06e-4f36-89ea-298e93a085e7', '51115469-3238-4bb8-a874-03480564d4c2')
